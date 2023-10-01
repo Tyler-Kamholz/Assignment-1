f3d4eec6eaa09250976ec62e761ec0535d039ed2
Keith Thoong <thoonk78@uwosh.edu>
2023-10-01 01:56:15 -0500

    Found the maximum profit in part A

--- Assignment 1/PartA.cs
+++ Assignment 1/PartA.cs
@@ -5,6 +5,60 @@ public class PartA
 {
 	public void Run()
 	{
+		int[] prices = { 4, 6, 9, 3, 2, 5, 8, 1 };
+		var max = FindMaxProfit(prices, 0, prices.Length - 1);
+		Console.WriteLine(max);
 	}
+
+	public static int FindMaxProfit(int[] arr, int left, int right)
+	{
+		int maxProfit = 0;
+
+		if (right - left <= 2)
+		{
+			return arr[right] - arr[left];
+		}
+
+		else
+		{
+			int mid = left + (right - left) / 2;
+			int firstHalfProfit = FindMaxProfit(arr, left, mid);
+			int secondHalfProfit = FindMaxProfit(arr, mid + 1, right);
+			int acrossMidProfit = findHighest(arr, mid + 1, right) - findLowest(arr, left, mid);
+			int[] results = {firstHalfProfit, secondHalfProfit, acrossMidProfit};
+
+			maxProfit = findHighest(results, 0, results.Length - 1);
+
+		}
+
+		return maxProfit;
+	}
+
+	public static int findLowest(int[] arr, int left, int right)
+	{
+		int lowest = int.MaxValue;
+
+		for (int i = left; i <= right; i++)
+		{
+			if (arr[i] < lowest)
+			{
+				lowest = arr[i];
+			}
+		}
+		return lowest;
+	}
+    public static int findHighest(int[] arr, int left, int right)
+    {
+        int highest = 0;
+
+        for (int i = left; i <= right; i++)
+        {
+            if (arr[i] > highest)
+            {
+                highest = arr[i];
+            }
+        }
+        return highest;
+    }
 }
 
